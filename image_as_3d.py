7782151df8c7a730d168a9501afd1c10010ae123
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2025-07-26 20:31:14 -0600

    Refactor `image_as_3d.py` to use `ImageAs3D` class, improving code organization and modularity
    - Encapsulate functionality into `ImageAs3D` class with `read_gray` and `convert_to_3d` methods.

--- image_as_3d.py
+++ image_as_3d.py
@@ -3,51 +3,58 @@ Created by greg-ogs at 07/16/2025
 '''
 
 import os
+from symtable import Class
+
 import skimage as ski
 from matplotlib import pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-def read_gray(img_path):
-    img = ski.io.imread(img_path)
-    img = ski.util.img_as_ubyte(img)
-    if len(img.shape) > 2:
-        img = ski.color.rgb2gray(img)
-    # plt.figure(figsize=(10, 8))
-    # plt.imshow(img)
-    # plt.axis('off')
-    # plt.show()
-    return img
+class ImageAs3D:
+    def __init__(self, image_path):
+        self.img_path = image_path
 
-def convert_to_3d(img):
-    stride = 10
-    # Create coordinate matrices
-    y, x = np.mgrid[0:img.shape[0], 0:img.shape[1]]
+    def read_gray(self):
+        img = ski.io.imread(self.img_path)
+        img = ski.util.img_as_ubyte(img)
+        if len(img.shape) > 2:
+            img = ski.color.rgb2gray(img)
+        # plt.figure(figsize=(10, 8))
+        # plt.imshow(img)
+        # plt.axis('off')
+        # plt.show()
+        return img
 
-    # Create 3D plot
-    fig = plt.figure(figsize=(20, 20))
-    ax = fig.add_subplot(111, projection='3d')
+    @staticmethod
+    def convert_to_3d(img):
+        stride = 10
+        # Create coordinate matrices
+        y, x = np.mgrid[0:img.shape[0], 0:img.shape[1]]
 
-    # Plot wireframe
-    # wire = ax.plot_wireframe(x[::stride, ::stride],
-    #                          y[::stride, ::stride],
-    #                          img[::stride, ::stride],
-    #                          color='red')
+        # Create 3D plot
+        fig = plt.figure(figsize=(20, 20))
+        ax = fig.add_subplot(111, projection='3d')
 
-    # Plot Surface
-    wire = ax.plot_surface(x[::stride, ::stride],
-                             y[::stride, ::stride],
-                             img[::stride, ::stride],
-                             color='#FFF000', cmap='viridis')
+        # Plot wireframe
+        # wire = ax.plot_wireframe(x[::stride, ::stride],
+        #                          y[::stride, ::stride],
+        #                          img[::stride, ::stride],
+        #                          color='red')
 
-    ax.set_xlabel('X')
-    ax.tick_params(axis='both', which='major', labelsize=30)
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Intensity')
+        # Plot Surface
+        wire = ax.plot_surface(x[::stride, ::stride],
+                                 y[::stride, ::stride],
+                                 img[::stride, ::stride],
+                                 color='#FFF000', cmap='viridis')
 
-    ax.view_init(elev=25, azim=280)
+        ax.set_xlabel('X')
+        ax.tick_params(axis='both', which='major', labelsize=30)
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Intensity')
 
-    plt.show()
+        ax.view_init(elev=25, azim=280)
+
+        plt.show()
 
 if __name__ == "__main__":
     image_files = []
@@ -66,9 +73,10 @@ if __name__ == "__main__":
         print(f"- {img}")
 
     for image_path in image_files:
-        image = read_gray(image_path)
-        convert_to_3d(image)
-
+        im3d = ImageAs3D(image_path)
+        image = im3d.read_gray()
+        im3d.convert_to_3d(image)
+        del im3d
 
 
 
